74e8db999858056374e9098b9ff8c5ebed863a3e
Binod2266 <binod_k@virtual-gravity.com>
2023-12-02 23:46:09 +0545

    Feat :  Dashboard Layout

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/sidebar";
+
+const Dashboard = () => {
+  return (
+    <div className="">
+      Home page dashboard. Add more designs here. Good Luck!!!
+    </div>
+  );
+};
+
+export default Dashboard;
