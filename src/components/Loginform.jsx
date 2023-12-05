63abd96d5077704926cc5ff364760e8820e05ca8
Sandhya <neupanesandhya4567@gamil.com>
2023-12-05 13:27:31 +0545

    recent  update on signup and login page

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -41,20 +41,20 @@ export default function Loginform() {
   return (
     <>
       <div className="h-screen w-screen  bg-primaryColor-100  max-sm:h-screen max-sm:w-screen flex flex-row max-sm:flex-col ">
-        <div className="flex justify-center   max-w-sm:flex-row w-1/3 bg-gradient-to-tl from-primaryColor-500 to-primaryColor-900 max-sm:w-full max-sm:h-1/2 ">
+        <div className="flex justify-center   max-w-sm:flex-row w-1/3 bg-gradient-to-tl from-primaryColor-500 to-primaryColor-900 max-sm:w-full max-sm:h-1/3 ">
           <div className=" text-primaryColor-100   self-center">
             <div className="flex justify-center ">
-              <label className="  font-bold font-mono text-3xl p-2 max-sm:text-2xl">
+              <label className="  font-bold font-mono text-3xl p-2 max-lg:text-2xl max-sm:text-xl">
                 New Here?
               </label>
             </div>
             <div className="flex justify-center">
-              <label className="font-bold font-mono text-lg text-center p-2 max-sm:text-xl ">
+              <label className="font-bold font-mono text-lg text-center p-2 max-sm:text-sm max-lg:text-xl ">
                 Revolutionize Attendance with Speed!
               </label>
             </div>
             <div className="flex justify-center mt-10 max-sm:mt-1">
-              <button className=" text-primaryColor-950  max-sm:text-sm  bg-primaryColor-100 rounded-3xl  font-medium tracking-wider uppercase text-lg py-2 mx-auto md:mx-10 w-[80%] md:w-[70%]  xl:w-[50%] mt-8 lg:mt-0  xl:ml-10 ">
+              <button className=" text-primaryColor-950  max-sm:text-sm  bg-primaryColor-100 rounded-3xl  font-medium tracking-wider uppercase text-lg py-2 mx-auto md:mx-10 w-[50%] md:w-[70%]  xl:w-[50%]  lg:mt-0  xl:ml-10 ">
                 <Link href="/signup">
                   <label>Sign Up</label>
                 </Link>
@@ -63,19 +63,24 @@ export default function Loginform() {
           </div>
         </div>
 
-        <div className="w-2/3  max-sm:w-full max-sm:h-1/2  ">
+        <div className="w-2/3  max-sm:w-full max-sm:h-2/3  ">
           <form onSubmit={handleSubmit}>
             <div className="h-5 w-full mb-3 text-center align-middle py-1">
-              <label className="text-center font-bold text-primaryColor-900 font-mono  max-lg:text-xl text-2xl">
+              <label className="text-center font-bold tracking-wide text-primaryColor-900 font-poppins max-lg:text-xl text-2xl">
                 LogIn
               </label>
             </div>
-            <div className=" p-10 w-full text-primaryColor-950 rounded-lg  mt-32 max-sm:mt-0 max-sm:w-full max-md:h-full  ">
+            <div className="h-6 w-full mb-2  text-center align-middle  ">
+              <label className="text-center font-thin tracking-wide text-primaryColor-600 font-poppins max-lg:text-xs text-sm">
+                Scan Smart, Attend Smarter
+              </label>
+            </div>
+            <div className=" px-20 max-sm:px-10 w-full text-primaryColor-950 rounded-lg  mt-28 max-sm:mt-0 max-sm:w-full max-md:h-full  ">
               {loginField.map(LoginData)}
 
               <div className="flex justify-center  mt-10 max-sm:mt-1    ">
                 <Button
-                  className=" text-primaryColor-50 font-medium max-sm:text-sm max-sm:py-1 max-sm:my-5 tracking-wider uppercase text-lg py-2 mx-auto max-lg:text-base md:mx-10 w-[80%] md:w-[70%]  xl:w-[50%] mt-8 lg:mt-0  xl:ml-10 "
+                  className=" text-primaryColor-50 font-medium max-sm:text-sm  max-sm:my-5 tracking-wider uppercase text-lg py-2 mx-auto max-lg:text-base md:mx-10 w-[80%] md:w-[70%]  xl:w-[50%] mt-8 lg:mt-0  xl:ml-10 "
                   buttonText="Login"
                 />
               </div>
--- src/components/Signupform.jsx
+++ src/components/Signupform.jsx
@@ -17,17 +17,17 @@ const inputField = [
 ];
 const Data = (item, index) => {
   return (
-    <div key={index} className=" max-lg:mb-1">
-      <div className="mb-2 max-md:mb-1 max-lg:mb-2 ">
+    <div key={index} className="">
+      <div className="mb-2 max-lg:mb-1  ">
         <label className="  max-sm:text-xs max-md:text-xs max-lg:text-sm text-primaryColor-950">
           {item.name}
         </label>
       </div>
-      <div className="flex mb-4 max-md:mb-2">
+      <div className="flex mb-3 max-lg:mb-1">
         <Input
           required
           type={item.type}
-          className="w-full   max-sm:text-xs max-md:text-xs max-lg:text-sm relative focus:border-primaryColor-700 focus:text-primaryColor-950 transition duration-200 input-type "
+          className="w-full    max-sm:text-xs max-md:text-xs max-lg:text-sm relative focus:border-primaryColor-700 focus:text-primaryColor-950 transition duration-200 input-type "
         />
         <label className="absolute mt-3 ml-3 max-sm:ml-3 max-sm:mt-2 max-md:text-xs max-lg:text-sm max-sm:text-xs     text-primaryColor-400 text-sm transition duration-200 input-text">
           {item.placeholder}
@@ -46,10 +46,10 @@ export default function Signupform() {
   return (
     <>
       <div className="h-screen w-screen bg-primaryColor-100  max-sm:h-screen max-sm:w-screen flex  ">
-        <div className=" flex w-1/3 bg-gradient-to-tl from-primaryColor-500 to-primaryColor-900 max-sm:hidden  ">
-          <div className="relative w-full h-full">
+        <div className=" flex w-1/3 bg-gradient-to-tl from-primaryColor-500 to-primaryColor-900 max-sm:hidden    ">
+          <div className="relative  w-full h-full">
             <Image
-              className="bg"
+              className="object-cover object-center"
               src="/images/bw.jpg"
               alt="qr code"
               layout="fill"
@@ -58,12 +58,17 @@ export default function Signupform() {
         </div>
         <div className="w-2/3 max-sm:w-full ">
           <form onSubmit={handleSubmit}>
-            <div className="h-5 w-full mb-3 text-center align-middle py-1">
-              <label className="text-center font-bold text-primaryColor-900 font-mono  max-lg:text-xl text-2xl">
+            <div className="h-8 w-full text-center  py-1 ">
+              <label className="text-center font-bold tracking-wide text-primaryColor-900 font-poppins max-lg:text-xl text-2xl">
                 SignUp
               </label>
             </div>
-            <div className="px-10 text-primaryColor-950 rounded-lg  max-sm:w-full max-sm:h-full max-md:h-full ">
+            <div className="h-6 w-full mb-2  text-center align-middle  ">
+              <label className="text-center font-thin tracking-wide text-primaryColor-600 font-poppins max-lg:text-xs text-sm">
+                Transform Attendance with a Scan
+              </label>
+            </div>
+            <div className="px-20 max-sm:px-10 text-primaryColor-950 rounded-lg  max-sm:w-full max-sm:h-full ">
               {inputField.map(Data)}
 
               <div className="flex flex-col max-lg:text-sm  max-sm:text-xs  mb-2">
@@ -71,7 +76,7 @@ export default function Signupform() {
                   <label>Role</label>
                 </div>
 
-                <div className="  max-md:text-xs  flex">
+                <div className="  max-md:text-xs py-1 flex">
                   <RadioGroup defaultValue="Teacher">
                     <div className="flex ">
                       <div className="flex items-center space-x-2  text-primaryColor-950 max-md:text-xs max-lg:text-sm">
@@ -87,7 +92,7 @@ export default function Signupform() {
                 </div>
               </div>
 
-              <div className="flex justify-center text-sm mb-2 max-lg:text-xs max-lg:mt-1">
+              <div className="flex justify-center mb-2 max-lg:mb-0 text-sm max-sm:mb-1  max-lg:text-xs max-lg:mt-0">
                 <label>
                   Already have an account?
                   <Link href="/login" className="underline font-semibold">
@@ -98,7 +103,7 @@ export default function Signupform() {
 
               <div className="flex justify-center  ">
                 <Button
-                  className=" text-primaryColor-200 align-middle  font-medium tracking-wider uppercase text-lg max-lg:text-base py-2  md:mx-10 w-[80%] md:w-[70%]  xl:w-[50%] mt-5 lg:mt-0  xl:ml-10   "
+                  className=" text-primaryColor-200 align-middle  font-medium tracking-wider uppercase text-lg max-lg:text-base py-2  md:mx-10 w-[80%] md:w-[70%]  xl:w-[50%] mt-4 lg:mt-0  xl:ml-10   "
                   buttonText="Sign Up"
                 />
               </div>
