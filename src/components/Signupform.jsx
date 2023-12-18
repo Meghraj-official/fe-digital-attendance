edd94c0496da992b98ce7f567e8a9bf930b10ffc
Sandhya <neupanesandhya4567@gamil.com>
2023-12-19 00:19:28 +0545

    login page updated

--- src/components/Signupform.jsx
+++ src/components/Signupform.jsx
@@ -53,65 +53,64 @@ export default function Signupform() {
   };
 
   return (
-    <>
-      <div className="h-screen w-screen bg-primaryColor-100  max-sm:h-screen max-sm:w-screen flex  ">
-        <div className=" flex w-1/3 bg-gradient-to-tl from-primaryColor-500 to-primaryColor-900 max-sm:hidden    ">
-          <div className="relative  w-full h-full">
-            <Image
-              className="object-cover object-center"
-              src="/images/bw.jpg"
-              alt="qr code"
-              layout="fill"
-            />
-          </div>
+    <div className="h-screen w-screen bg-primaryColor-100  max-sm:h-screen max-sm:w-screen flex  ">
+      <div className=" flex w-1/3 bg-gradient-to-tl from-primaryColor-500 to-primaryColor-900 max-sm:hidden    ">
+        <div className="relative  w-full h-full">
+          <Image
+            className="object-cover object-center"
+            src="/images/bw.jpg"
+            alt="qr code"
+            layout="fill"
+          />
         </div>
-        <div className="w-2/3 max-sm:w-full ">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="h-8 w-full text-center  py-1 ">
-              <label className="text-center font-bold tracking-wide text-primaryColor-900 font-poppins max-lg:text-xl text-2xl">
-                SignUp
-              </label>
-            </div>
-            <div className="h-6 w-full mb-2  text-center align-middle  ">
-              <label className="text-center font-thin tracking-wide text-primaryColor-600 font-poppins max-lg:text-xs text-sm">
-                Transform Attendance with a Scan
-              </label>
-            </div>
-            <div className="px-40 max-sm:px-10 max-lg:px-20 text-primaryColor-950 rounded-lg  max-sm:w-full max-sm:h-full ">
-              {inputField.map((item) => (
-                <Data register={register} item={item} key={item.name} />
-              ))}
+      </div>
+      <div className="w-2/3 max-sm:w-full ">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="h-8 w-full text-center  py-1 ">
+            <label className="text-center font-bold tracking-wide text-primaryColor-900 font-poppins max-lg:text-xl text-2xl">
+              SignUp
+            </label>
+          </div>
+          <div className="h-6 w-full mb-2  text-center align-middle  ">
+            <label className="text-center font-thin tracking-wide text-primaryColor-600 font-poppins max-lg:text-xs text-sm">
+              Transform Attendance with a Scan
+            </label>
+          </div>
+          <div className="px-40 max-sm:px-10 max-lg:px-20 text-primaryColor-950 rounded-lg  max-sm:w-full max-sm:h-full ">
+            {inputField.map((item) => (
+              <Data register={register} item={item} key={item.name} />
+            ))}
 
             <div className="flex flex-col max-lg:text-sm  max-sm:text-xs  mb-2">
               <div className=" max-md:text-xs ">
                 <label>Role</label>
               </div>
 
-                <div className="  max-md:text-xs py-1 flex">
-                  <RadioGroup defaultValue="Teacher">
-                    <div className="flex ">
-                      <div className="flex items-center space-x-2  text-primaryColor-950 max-md:text-xs max-lg:text-sm">
-                        <input
-                          type="radio"
-                          name={"Role"}
-                          {...register("Role")}
-                          value={"Teacher"}
-                        />
-                        Teacher
-                      </div>
-                      <div className="flex items-center space-x-2 ml-10 max-sm:ml-5   text-primaryColor-950 max-md:text-xs max-lg:text-sm">
-                        <input
-                          type="radio"
-                          name={"gender"}
-                          {...register("Role")}
-                          value={"Student"}
-                        />
-                        Student
-                      </div>
+              <div className="  max-md:text-xs py-1 flex">
+                <RadioGroup defaultValue="Teacher">
+                  <div className="flex ">
+                    <div className="flex items-center space-x-2  text-primaryColor-950 max-md:text-xs max-lg:text-sm">
+                      <input
+                        type="radio"
+                        name={"Role"}
+                        {...register("Role")}
+                        value={"Teacher"}
+                      />
+                      Teacher
                     </div>
-                  </RadioGroup>
-                </div>
+                    <div className="flex items-center space-x-2 ml-10 max-sm:ml-5   text-primaryColor-950 max-md:text-xs max-lg:text-sm">
+                      <input
+                        type="radio"
+                        name={"gender"}
+                        {...register("Role")}
+                        value={"Student"}
+                      />
+                      Student
+                    </div>
+                  </div>
+                </RadioGroup>
               </div>
+            </div>
 
             <div className="flex justify-center mb-2 max-lg:mb-0 text-sm max-sm:mb-1  max-lg:text-xs max-lg:mt-0">
               <label>
@@ -122,15 +121,14 @@ export default function Signupform() {
               </label>
             </div>
 
-              <div className="flex justify-center  ">
-                <Button
-                  className=" text-primaryColor-200 align-middle  font-medium tracking-wider uppercase text-lg max-lg:text-base py-2  md:mx-10 w-[80%] md:w-[70%]  xl:w-[50%] mt-4 lg:mt-0  xl:ml-10   "
-                  buttonText="Sign Up"
-                />
-              </div>
+            <div className="flex justify-center  ">
+              <Button
+                className=" text-primaryColor-200 align-middle  font-medium tracking-wider uppercase text-lg max-lg:text-base py-2  md:mx-10 w-[80%] md:w-[70%]  xl:w-[50%] mt-4 lg:mt-0  xl:ml-10   "
+                buttonText="Sign Up"
+              />
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   );
