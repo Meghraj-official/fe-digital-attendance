6668ad60d83a81742ecb27fa03ae7d3b560d95f5
Sandhya <neupanesandhya4567@gamil.com>
2023-12-19 14:07:57 +0545

    final commit on dashboard branch

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,13 +2,12 @@ import Link from "next/link";
 import React from "react";
 import Loginform from "../../../components/Loginform";
 
-
-
 const Login = () => {
-  return <div className="">
-    <Link href='/login'><Loginform/></Link>
-    
-       </div>;
+  return (
+    <div className="">
+      <Loginform />
+    </div>
+  );
 };
 
 export default Login;
