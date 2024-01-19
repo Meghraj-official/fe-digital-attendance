64c10455d9e5656709d7def2884e294caa60423e
Binod2266 <binod_k@virtual-gravity.com>
2024-01-20 03:27:15 +0545

    API : Api integration on admin dashboard pending user and students

--- src/app/dashboard/admin/pendingusers/page.jsx
+++ src/app/dashboard/admin/pendingusers/page.jsx
@@ -1,11 +1,50 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import { PendingTeacher } from "@/components/dashboard/admin/PendingTeacher";
 import { PendingStudent } from "@/components/dashboard/admin/PendingStudent";
-import React, { useState } from "react";
+import axiosInstance from "@/lib/axios";
+import { useQuery } from "react-query";
+import { useAuthStore } from "@/store/authStore";
 
 const tab = ["teacher", "student"];
+
 const PendingUser = () => {
+  const { token } = useAuthStore();
   const [activeTab, setActiveTab] = useState("teacher");
+
+  const handleGetPendingStudents = () => {
+    const res = axiosInstance.get("/user/pending-students", {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    return res;
+  };
+  const handleGetPendingTeachers = () => {
+    const res = axiosInstance.get("/user/pending-teachers", {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    return res;
+  };
+
+  useEffect(() => {
+    handleGetPendingStudents();
+    handleGetPendingTeachers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const { data, refetch, isLoading } = useQuery(
+    "pendingStudents",
+    handleGetPendingStudents
+  );
+  const {
+    data: teachersData,
+    refetch: refetchTeacher,
+    isLoading: isPending,
+  } = useQuery("pendingTeachers", handleGetPendingTeachers);
 
   return (
     <div className="w-full h-full flex flex-col justify-center">
@@ -24,10 +63,21 @@ const PendingUser = () => {
           </button>
         ))}
       </div>
-
       <div className="h-[70vh] w-[95%] mt-10 ml-10 overflow-y-auto  bg-primaryColor-100">
-        {activeTab === "teacher" && <PendingTeacher />}
-        {activeTab === "student" && <PendingStudent />}
+        {activeTab === "teacher" && (
+          <PendingTeacher
+            isLoading={isPending}
+            refetch={refetchTeacher}
+            teacherData={teachersData?.data?.teachers}
+          />
+        )}
+        {activeTab === "student" && (
+          <PendingStudent
+            isLoading={isLoading}
+            refetch={refetch}
+            studentData={data?.data?.students}
+          />
+        )}
       </div>
     </div>
   );
