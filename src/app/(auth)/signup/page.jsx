a2d753e0e40e93f2b842c59c6b606287aace1981
Binod2266 <binod_k@virtual-gravity.com>
2024-01-21 22:40:51 +0545

    Feat:Implemented Protected routes on login signup and home page

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -3,12 +3,17 @@ import React from "react";
 import Signupform from "@/components/Signup/Signupform";
 import { useSearchParams } from "next/navigation";
 import SignupStudentForm from "@/components/signupstudent/SignupStudentForm";
+import AuthWrapper from "@/components/common/AuthWrapper";
 const Signup = () => {
   const searchParams = useSearchParams();
 
   const type = searchParams.get("type");
 
-  return <>{type === "teacher" ? <Signupform /> : <SignupStudentForm />}</>;
+  return (
+    <AuthWrapper>
+      {type === "teacher" ? <Signupform /> : <SignupStudentForm />}
+    </AuthWrapper>
+  );
 };
 
 export default Signup;
