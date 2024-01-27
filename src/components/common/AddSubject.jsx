c694926ca638d98be494509e0b911d9325060a2a
Binod2266 <binod_k@virtual-gravity.com>
2024-01-27 23:45:51 +0545

    Feat: build create subject modal form

--- src/components/common/AddSubject.jsx
+++ src/components/common/AddSubject.jsx
@@ -6,16 +6,65 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import axiosInstance from "@/lib/axios";
 import { useMutation } from "react-query";
+import CustomInput from "../dashboard/common/CustomInput";
+import Button from "./Button";
+import Selector from "./Selector";
+import RhfSelect from "../reactHookForms/RhfSelect";
+import { semesterOptions, yearOptions } from "@/lib/data/signup";
+
+const AllCourses = [
+  {
+    _id: "65a988d3242f4a886b328207",
+    name: "Bachelors in Business Administration",
+    code: "BSC CSIT",
+    courseType: "YEARLY",
+    __v: 0,
+    years: {
+      first: ["CSC3"],
+      second: ["CSC3"],
+      third: ["CSC3"],
+      fourth: ["CSC3"],
+      _id: "65abe856ad4f513623f89f34",
+    },
+  },
+  {
+    _id: "65a98e576d88f5b723d37d02",
+    name: "Bachelors in Business Administration",
+    code: "BBA",
+    courseType: "SEMESTER",
+    semesters: {
+      first: ["CSC3"],
+      second: ["CSC3"],
+      third: ["CSC3"],
+      fourth: ["CSC3"],
+      fifth: ["CSC3"],
+      sixth: ["CSC3"],
+      seventh: ["CSC3"],
+      eighth: ["CSC3"],
+      _id: "65a98e576d88f5b723d37d03",
+    },
+    __v: 0,
+  },
+];
 
 const AddSubject = () => {
+  // { resolver: yupResolver(createCourseSchema) }
+  const methods = useForm();
+  const {
+    handleSubmit,
+    watch,
+    // formState: { isDirty },
+  } = methods;
   const { mutate } = useMutation(async (body) => {
     return await axiosInstance.post("/subject/create", body);
   });
+  const course = watch("course");
+  const courseType = course && JSON.parse(course).courseType;
+  const isYearly = courseType === "YEARLY";
 
-  const { register, handleSubmit } = useForm();
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -26,48 +75,44 @@ const AddSubject = () => {
       <DialogContent className="sm:max-w-[425px] bg-primaryColor-200">
         <DialogHeader>
           <DialogTitle>Add Subject</DialogTitle>
-          {/* <DialogDescription>
-            Add student's data manually. Click save when you're done.
-          </DialogDescription> */}
         </DialogHeader>
-        <form
-          onSubmit={handleSubmit((data) => {
-            console.log("hiiii", data);
-            mutate(data);
-          })}
-        >
-          {" "}
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="name" className="text-right text-sm">
-                Subject Name
-              </label>
-              <input
-                {...register("name")}
-                id="name"
-                className="col-span-3 outline-none h-6 text-sm"
+        <FormProvider {...methods}>
+          <form
+            onSubmit={handleSubmit((data) => {
+              const appendData = JSON.parse(data.course);
+              const formatData = { ...data, ...appendData };
+              formatData.courseType === "YEARLY"
+                ? delete formatData?.semester
+                : delete formatData?.year;
+              delete formatData.course;
+              mutate(data);
+            })}
+          >
+            {" "}
+            <div className="flex flex-col gap-3">
+              <CustomInput name="name" id="name" labelName="Subject Name" />
+              <CustomInput name="code" id="code" labelName="Subject Code" />
+              <Selector
+                name="course"
+                labelName="Select Course"
+                placeholder="Choose course"
+                options={AllCourses}
+              />
+              <RhfSelect
+                placeholder={isYearly ? "Select Year" : "Select Semester"}
+                name={isYearly ? "year" : "semester"}
+                options={isYearly ? yearOptions : semesterOptions}
               />
             </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="ID" className="text-right text-sm">
-                Subject Code
-              </label>
-              <input
-                {...register("code")}
-                id="id"
-                className="col-span-3 h-6 outline-none text-sm "
+            <DialogFooter>
+              <Button
+                buttonText="Add Subject"
+                type="submit"
+                className="w-full py-2 text-white mt-3  "
               />
-            </div>
-          </div>
-          <DialogFooter>
-            <button
-              type="submit "
-              className="bg-primaryColor-800 hover:bg-primaryColor-600 border-1 rounded-md p-2 text-primaryColor-50"
-            >
-              Add
-            </button>
-          </DialogFooter>
-        </form>
+            </DialogFooter>
+          </form>
+        </FormProvider>
       </DialogContent>
     </Dialog>
   );
