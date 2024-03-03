b375da26469ce6b239f03d032ccd383cae857c68
Manisha1107 <manisapkhrl@gmail.com>
2024-03-03 13:11:46 +0545

    style:student layout designed for mobile view

--- src/components/dashboard/student/Info.jsx
+++ src/components/dashboard/student/Info.jsx
@@ -4,9 +4,9 @@ const levels ={
 const Info=({data})=>{
     return(
         <>
-        <div className="border border-primaryColor-600 bg-primaryColor-100 min-w-fit max-w-[200px]  rounded-lg py-4 px-2 flex flex-col gap-3 lg:p-7 ">
-            <h1 className="text-center w-full rounded-md text-lg lg:text-xl font-poppins font-semibold ">Student Info</h1>
-           <div className="flex flex-col text-start  gap-2 text-base lg:text-lg p-2 font-poppins "> 
+        <div className="border border-primaryColor-600 bg-primaryColor-100 min-w-fit max-w-[200px] rounded-lg py-3 px-2  flex flex-col h-min justify-center gap-1 lg:gap-3 lg:p-6">
+            <h1 className="text-center w-full rounded-md text-sm lg:text-xl font-poppins font-semibold ">Student Info</h1>
+           <div className="flex flex-col text-start  gap-2 text-sm lg:text-lg p-2 font-poppins "> 
             <label >Name: {data?.fullName}</label>
             <label className="capitalize">Level: {data?.year?`${levels[data?.year]} year`:`${levels[data?.semester]} semester`}</label>
             <label>Roll no: {data?.rollNo}</label>
