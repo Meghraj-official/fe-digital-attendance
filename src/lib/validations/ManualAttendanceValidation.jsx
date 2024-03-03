c4b06803cdf85e184191d0b13e51a556f470438f
Sandhya <neupanesandhya4567@gamil.com>
2024-03-03 19:02:39 +0545

    fix:validation in manual attendance

--- src/lib/validations/ManualAttendanceValidation.jsx
+++ src/lib/validations/ManualAttendanceValidation.jsx
@@ -1,11 +1,17 @@
 import * as yup from "yup";
 
 export const manualAttendanceSchema = yup.object({
-  section: yup.string().required("Please Choose Section "),
+  section: yup
+    .string()
+    .required("Section is required")
+    .length(1)
+    .matches(/^[a-zA-Z]+$/, "Section must be a single string"),
   batch: yup.string().required("Please Choose Batch "),
   rollNo: yup
     .number()
+    .integer()
     .typeError("Roll number must be a number")
-    .required("Please enter student roll no"),
+    .required("Please enter student roll no")
+    .integer(),
   subjectCode: yup.string().required("Please Subject Code "),
 });
