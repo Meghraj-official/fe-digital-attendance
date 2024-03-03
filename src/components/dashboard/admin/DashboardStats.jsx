e28aed863151fb90f9dfe87d84c9b1a9eb7b712d
Manisha1107 <manisapkhrl@gmail.com>
2024-03-04 00:10:09 +0545

    feat:some feature added in admin-dashboard

--- src/components/dashboard/admin/DashboardStats.jsx
+++ src/components/dashboard/admin/DashboardStats.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
-import { Item } from "@radix-ui/react-radio-group";
-import { BookCopyIcon, BookOpenIcon, BookUpIcon, GraduationCap, User2Icon, UserCircle2Icon, UserCog2Icon, UserSquare2Icon } from "lucide-react";
+
+import { BookCopyIcon,BookOpenIcon, GraduationCap, User2Icon} from "lucide-react";
 const data=[
   {
     title:"Total Students",
@@ -34,19 +34,19 @@ const data=[
 export default function DashboardStats() {
   return (
     <div className="grid grid-cols-2 gap-10 lg:h-[80vh] px-3 ">
-      {data.map(Item=>{
+      {data.map((item,index)=>{
         return(
-          <Card>
+          <Card key={index}>
           <CardHeader className="flex flex-row items-center justify-between h-[20%] pb-2 space-y-0">
             <CardTitle className="text-base font-semibold">
-            {Item.title}
+            {item.title}
             </CardTitle>
-            {Item.icon}
+            {item.icon}
           </CardHeader>
           <CardContent className=" flex flex-col justify-center h-[80%] ">
-            <div className="text-5xl font-bold">{Item.count}</div>
+            <div className="text-5xl font-bold">{item.count}</div>
             <p className="text-sm  dark:text-gray-400">
-              {Item.description}
+              {item.description}
             </p>
           </CardContent>
         </Card>
